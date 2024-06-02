ac5e53a629bd4345992b075bae407c63b5a9a590
JonyHuergo <jh2000@hotmail.com.ar>
2024-06-02 13:41:30 -0300

    Agregar enpoint para obtener todas las cabañas

--- backend/app.py
+++ backend/app.py
@@ -13,6 +13,12 @@ class TablaEjemplo(db.Model):
     nombre = db.Column(db.String(255))
     edad = db.Column(db.Integer)
 
+class Cabañas(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(255))
+    descripcion = db.Column(db.Text)
+    imagen = db.Column(db.Text)
+
 @app.route('/datos_ejemplo')
 def obtener_datos_ejemplo():
     # Consultar todos los registros de la tabla de ejemplo
@@ -36,6 +42,22 @@ def obtener_datos_ejemplo():
 @app.route('/')
 def hello_world():
     return '<h2> Welcome to the backend </h2> <a href="/datos_ejemplo"> <button> Ver Prueba </button> </a>'
+
+@app.route('/cabañas')
+def get_all_cabins():
+    cabañas = Cabañas.query.all()
+
+    results = []
+    for cabaña in cabañas:
+        result = {
+            'id': cabaña.id,
+            'nombre': cabaña.nombre,
+            'descripcion': cabaña.descripcion,
+            'imagen': cabaña.imagen
+        }
+        results.append(result)
+
+    return jsonify(results)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001)
