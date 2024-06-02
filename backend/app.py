64ebda272a5f4beb02c1ad62dba9d24fa4e1e1db
JonyHuergo <jh2000@hotmail.com.ar>
2024-06-02 14:56:00 -0300

    Remover datos ejemplo

--- backend/app.py
+++ backend/app.py
@@ -8,11 +8,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
-class TablaEjemplo(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    nombre = db.Column(db.String(255))
-    edad = db.Column(db.Integer)
-
 class Cabañas(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nombre = db.Column(db.String(255))
@@ -26,30 +21,6 @@ class Habitaciones(db.Model):
     descripcion = db.Column(db.Text)
     precio = db.Column(db.Numeric(10,2))
     imagen = db.Column(db.Text)
-
-@app.route('/datos_ejemplo')
-def obtener_datos_ejemplo():
-    # Consultar todos los registros de la tabla de ejemplo
-    datos = TablaEjemplo.query.all()
-
-    # Crear una lista para almacenar los resultados
-    resultados = []
-
-    # Iterar sobre los registros y agregarlos a la lista de resultados
-    for dato in datos:
-        resultado = {
-            'id': dato.id,
-            'nombre': dato.nombre,
-            'edad': dato.edad
-        }
-        resultados.append(resultado)
-
-    # Devolver los resultados como respuesta JSON
-    return jsonify(resultados)
-
-@app.route('/')
-def hello_world():
-    return '<h2> Welcome to the backend </h2> <a href="/datos_ejemplo"> <button> Ver Prueba </button> </a>'
 
 @app.route('/cabañas')
 def get_all_cabins():
--- init-db/init.sql
+++ init-db/init.sql
@@ -1,12 +1,3 @@
-CREATE TABLE IF NOT EXISTS tabla_ejemplo(
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nombre VARCHAR(255) NOT NULL,
-    edad INT NOT NULL
-);
-INSERT INTO tabla_ejemplo (nombre, edad) VALUES
-('Juan Perez', 30),
-('Maria Garcia', 25),
-('Carlos Sanchez', 35);
 CREATE TABLE IF NOT EXISTS cabañas(
     id INT AUTO_INCREMENT PRIMARY KEY,
     nombre VARCHAR(255) NOT NULL,
